7f4378e238701379c03f7b499e990b88b0f35ed7
Revathi-1204 <72979223+Revathi-1204@users.noreply.github.com>
2023-09-07 15:28:43 +0530

    Add files via upload

---
+++ Post-Like-Comments-main/Post-Like-Comments-main/learningspring/src/main/java/com/prodapt/learningspring/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.prodapt.learningspring.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name="Comments")
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	  
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "author_id", referencedColumnName = "id")
+	private User user;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "post_id", referencedColumnName = "id")
+	private Post post;
+
+	private Date createdAtTime;
+	
+}
